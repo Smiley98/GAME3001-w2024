70eae5004bde41585a8d57b93f330bdd35e84cc9
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-23 14:59:08 -0500

    Review and vector math proofs

--- GAME3001-02/Assets/_MyAssets/_Scripts/AgentMovement.cs
+++ GAME3001-02/Assets/_MyAssets/_Scripts/AgentMovement.cs
@@ -8,19 +8,37 @@ public class AgentMovement : MonoBehaviour
     [SerializeField] private float moveSpeed;
     private Vector3 targetPosition = Vector3.zero;
 
+    void Start()
+    {
+        Vector2 A = new Vector2(2.0f, 3.0f);    // Start position
+        Vector2 B = new Vector2(10.0f, 15.0f);  // End position
+        Vector2 AB = B - A;                     // Line from start to end
+        Vector2 N1 = AB.normalized;
+        Vector2 N2 = AB / Mathf.Sqrt(AB.x * AB.x + AB.y * AB.y);
+        float length1 = N1.magnitude;
+        float length2 = N2.magnitude;
+        Debug.Log(N1);  // Automatic normalization
+        Debug.Log(N2);  // Manual normalization
+        Debug.Log(length1);
+        Debug.Log(length2);
+    }
+
     void Update()
     {
-        
         // Check for mouse input.
         if (Input.GetMouseButton(0))
         {
             // Convert mouse position to world position.
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            targetPosition.z = 0f; // Ensure the Z-coordinate is correct for a 2D game  .     
+            targetPosition.z = 0.0f; // Ensure the Z-coordinate is correct for a 2D game  .     
         }
 
         // Move towards the target position.
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        // Manual implementation of the above
+        //float translation = Mathf.Min(moveSpeed * Time.deltaTime, (targetPosition - transform.position).magnitude);
+        //transform.position += (targetPosition - transform.position).normalized * translation;
 
         // Rotate to look at the target position.
         LookAt2D(targetPosition);
